bd17141895bb616fb4e8ed1ce405f67f646685ed
Jerel <65048179+jerels@users.noreply.github.com>
2021-01-13 16:19:54 -0600

    finished variables

--- site.php
+++ site.php
@@ -7,9 +7,13 @@
 </head>
 <body>
     <?php
-        echo("<h1>Jerel's Site</h1>");
-        echo '<hr>';
-        echo '<p>This is my site</p>';
+        $charName = 'John';
+        $charAge = 35;
+
+        echo "There once was a man named $charName <br>";
+        echo "He was $charAge years old <br>";
+        echo "He really liked the name $charName <br>";
+        echo "But didn't like being $charAge <br>";
     ?>
 </body>
 </html>
